bfc24c3f19e48101a409746c79dd764ec23958b3
veroniquepelletier <pelletierv@hotmail.com>
2013-01-23 22:03:03 -0500

    new test

---
+++ infinite_scroll/app.js
@@ -0,0 +1,12 @@
+angular.module('app', [])
+    .service('model', function() {
+        var data = [];
+        
+    })
+    .directive('ifscroll', function() {
+        
+    })
+    .controller('scrollCtrl', function($scope, model) {
+        console.log('new scrollCtrl')
+        var scroll = new iScroll('wrapper', { hScrollbar: false, vScrollbar: false });
+    })
